8439bfbd1081596f5cd9aa26787fc539b4667cf4
saadkhan6670IX <asad.khan@integrationxperts.com>
2022-10-05 12:43:42 +0500

    added home route

--- component/Navbar.tsx
+++ component/Navbar.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 const Navbar = () => {
     return ( <nav>
-        <div className="logo">
+        <Link href={"/"} className="logo">
 <Image src="/logo.png" width={128} height={77}/>
-        </div>
+        </Link>
        <Link href={"/"}><a>Home</a></Link>
        <Link href={"/about"}><a>About</a></Link> 
        <Link href={"/ninjas"}><a>Ninjas</a></Link> 
